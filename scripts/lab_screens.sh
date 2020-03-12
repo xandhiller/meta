b67d44249bb832de3f9e12493f7041eaf047cc59
Alex Hiller <xandhiller@gmail.com>
2020-03-12 15:41:16 +1100

    Added my references to the tex templates section.

---
+++ scripts/lab_screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1600x900 --pos 1280x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --mode 1280x1024 --pos 0x0 --rotate normal --output HDMI-2 --off
